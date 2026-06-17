6fccb935b1a30d691e51ae6d2cb6f03200ca6f86
sambhuyoungyanghang <mr.sambhu.young@gmail.com>
2026-06-17 02:35:51 +0100

    feat: management command to setup new restaurant's menu

--- orders/admin.py
+++ orders/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
+# from .models import Table
+from .models import Table, Category, MenuItem
 
-# Register your models here.
-from django.contrib import admin
-from .models import Table
-
-# Register your models here.
 @admin.register(Table)
 class TableAdmin(admin.ModelAdmin):
+    # list_display = ["name", "is_reserved"]
     list_display = ["name", "is_reserved"]
 
-
+admin.site.register(Category)
+admin.site.register(MenuItem)

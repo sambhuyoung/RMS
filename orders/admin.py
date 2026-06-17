6e5452f7f32bb0d97d4e3d98816f3908b18d68cc
sambhuyoungyanghang <mr.sambhu.young@gmail.com>
2026-06-17 23:51:46 +0100

    feat: db tables config for order and order items.

--- orders/admin.py
+++ orders/admin.py
@@ -1,11 +1,14 @@
+
 from django.contrib import admin
-# from .models import Table
 from .models import Table, Category, MenuItem
+from .models import Table, Category, MenuItem, Order, OrderItem
 
 @admin.register(Table)
 class TableAdmin(admin.ModelAdmin):
-    # list_display = ["name", "is_reserved"]
     list_display = ["name", "is_reserved"]
+
 
 admin.site.register(Category)
 admin.site.register(MenuItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 
 class Table(models.Model):
@@ -27,3 +28,24 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return self.name
+        return self.name
+
+
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    table = models.ForeignKey(Table, on_delete=models.PROTECT, related_name="orders")
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.table} -> {self.created_at}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name="items")
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.PROTECT, related_name="order_items")
+    price = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.menu_item}  x {self.quantity} qtys"

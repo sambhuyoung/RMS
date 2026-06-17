be2a77162ef66f41607447049b88dc6d1a66d70c
sambhuyoungyanghang <mr.sambhu.young@gmail.com>
2026-06-17 23:57:10 +0100

    feat: implement place order logic in view.

--- orders/views.py
+++ orders/views.py
@@ -1,21 +1,52 @@
-from django.shortcuts import render
-# from django.contrib.auth.decorators import login_required
+# from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .decorators import role_required
 from accounts.models import User
-from .models import Table, Category
+# from .models import Table, Category
+from .models import Table, Category, Order, OrderItem, MenuItem
+import json
+from django.contrib import messages
 
 
 @role_required([User.ROLE_CHOICES.WAITER])
 def tables_view(request):
-    # return render(request, "orders/tables.html")
     tables = Table.objects.all()
 
     return render(request, "orders/tables.html", {
         'tables': tables
     })
 
+
 @role_required([User.ROLE_CHOICES.WAITER])
 def menu_view(request, table_id):
+    if request.method == "POST":
+        data = request.POST.get("order-items")
+        data_dict = json.loads(data)
+        # {
+        #     'table_id': '26',
+        #     'items': [{'item_id': '1', 'quantity': 1},
+        #            {'item_id': '2', 'quantity': 1}
+        #            ]
+        #  }
+        table_id = data_dict.get('table_id')
+        table_obj = Table.objects.get(pk=table_id)
+        order = Order.objects.create(
+            table=table_obj
+        )
+
+        for orderitem in data_dict.get('items'):
+            menu_item = MenuItem.objects.get(pk=orderitem.get('item_id'))
+            OrderItem.objects.create(
+                order=order,
+                menu_item=menu_item,
+                price=menu_item.price,
+                quantity=orderitem.get('quantity')
+            )
+
+        messages.success(request, "Order created successfully")
+
+        return redirect("tables_view_url")
+
     categories = Category.objects.all()
 
     return render(request, "orders/menu.html", {

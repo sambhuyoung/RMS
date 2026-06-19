b490749dbd760ee4ead245b041b37fec1bc5158c
sambhuyoungyanghang <mr.sambhu.young@gmail.com>
2026-06-19 13:40:23 +0100

    feat: status tracking in order table

--- orders/models.py
+++ orders/models.py
@@ -28,12 +28,18 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return self.name
-        return self.name
 
 
 class Order(models.Model):
+    class ORDER_STATUS(models.TextChoices):
+        PREPARING = 'p', 'Preparing'
+        READY = 'r', 'Ready'
+        SERVED = 's', 'Served'
+        BILLED = 'b', 'Billed'
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     table = models.ForeignKey(Table, on_delete=models.PROTECT, related_name="orders")
+    status = models.CharField(max_length=2, choices=ORDER_STATUS, default=ORDER_STATUS.PREPARING)
 
     created_at = models.DateTimeField(auto_now_add=True)
 

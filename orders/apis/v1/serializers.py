d756db6e1a2b5d925ce0f31383ef1a3c0e03e8db
sambhuyoungyanghang <mr.sambhu.young@gmail.com>
2026-07-01 02:18:39 +0100

    API: Now it's also fetching the date from the Kitchen Station.

--- orders/apis/v1/serializers.py
+++ orders/apis/v1/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from orders.models import MenuItem, Table, Category
+from orders.models import MenuItem, Table, Category, KitchenStation
 
+
+class KitchenStationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = KitchenStation
+        fields = '__all__'
 
 class MenuItemSerializer(serializers.ModelSerializer):
+    station = KitchenStationSerializer()
+    class Meta:
+        model = MenuItem
     class Meta:
         model = MenuItem
         fields = '__all__'
@@ -16,10 +24,10 @@ class TableSerializer(serializers.ModelSerializer):
 
 class CategorySerializer(serializers.ModelSerializer):
 
-    # also give Items making it read only not modifiyable.
+    # also give Items making it read only not chageable.
     #items = MenuItemSerializer(many=True, read_only=True)
 
-    # giving different name than in relation name
+    # giving different name than in relation name inbetween MenuItems and Category.
     menuitems = MenuItemSerializer(source='items', many = True, read_only=True)
 
     class Meta:

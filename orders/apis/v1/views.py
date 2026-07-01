5174a2a7590c6c9fff25f4a4da23b577aecbbdc5
sambhuyoungyanghang <mr.sambhu.young@gmail.com>
2026-07-02 00:47:26 +0100

    API-Pagination: Pagination applied on menuitems. Note there won't be any visible difference until '?limit=10' is added at the end of URL. ?limit=10 will display only 10 menuitems at a time.

--- orders/apis/v1/views.py
+++ orders/apis/v1/views.py
@@ -6,11 +6,17 @@ from orders.models import MenuItem, Table, Category
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import MenuItemSerializer, TableSerializer, CategorySerializer
+from rest_framework.pagination import LimitOffsetPagination
+
 
 
 class MenuItemViewSet(viewsets.ModelViewSet):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    # For pagination
+    pagination_class = LimitOffsetPagination # There won't be any difference until write on ULR '?limit=10
+    # view 'https://www.django-rest-framework.org/api-guide/pagination/'
+
 
 
 class TableViewSet(viewsets.ModelViewSet):
